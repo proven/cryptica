9d8515c74b31b8b17c086d39d831b14abd9da7e1
Rod Hynes <rod.hynes@provensecuritysolutions.com>
2012-03-23 18:19:38 -0400

    TLS, mutual authentication, custom CA

--- client/client.py
+++ client/client.py
@@ -3,6 +3,7 @@ import socketio
 import socketio.namespace
 import socketio.mixins
 import ws4py.client.geventclient
+import ssl
 
 
 class WebSocketBackEnd(ws4py.client.geventclient.WebSocketClient):
@@ -15,6 +16,20 @@ class WebSocketBackEnd(ws4py.client.geventclient.WebSocketClient):
     def process_response_line(self, response_line):
         print 'process_response_line: ' + response_line
         ws4py.client.geventclient.WebSocketClient.process_response_line(self, response_line)
+
+    def connect(self):
+        # Hack to rewrite just the first part of WebSocketBaseClient.connect (wrap_socket)
+        self.url = 'not-wss://127.0.0.1:9000'
+        self.sock = ssl.wrap_socket(
+                        self.sock,
+                        certfile='../pki/data/certs/clientcert.pem',
+                        keyfile='../pki/data/private/clientkey.pem',
+                        ca_certs='../pki/data/certs/cacert.pem',
+                        cert_reqs=ssl.CERT_REQUIRED,
+                        ssl_version=ssl.PROTOCOL_TLSv1)
+        self.sender = self.sock.sendall
+        # Now call the rest of the function
+        ws4py.client.geventclient.WebSocketClient.connect(self)
 
     def opened(self):
         print 'WebSocketBackEnd connected'
@@ -45,8 +60,7 @@ class SocketIOFrontEnd(socketio.namespace.BaseNamespace):
     def recv_connect(self):
         print 'SocketIOFrontEnd connecting...'
         # TODO: instance per connection?
-        self.ws = WebSocketBackEnd('ws://127.0.0.1:9000', self)
-        self.ws.connect()
+        # self.ws = ...
         print 'SocketIOFrontEnd connected'
 
     def recv_disconnect(self):
@@ -57,8 +71,8 @@ class SocketIOFrontEnd(socketio.namespace.BaseNamespace):
     def on_message(self, message):
         print 'SocketIOFrontEnd sending: ' + message
         if not hasattr(self, 'ws'):
-            self.ws = WebSocketBackEnd('ws://127.0.0.1:9000', self)
-            self.ws.connect()            
+            self.ws = WebSocketBackEnd('wss://127.0.0.1:9000', self)
+            self.ws.connect()
         self.ws.send(message)
 
 
--- server/server.py
+++ server/server.py
@@ -1,6 +1,7 @@
 from gevent import monkey; monkey.patch_all()
 import ws4py.websocket
 import ws4py.server.geventserver
+import ssl
 
 
 class Server(ws4py.websocket.WebSocket):
@@ -13,5 +14,10 @@ class Server(ws4py.websocket.WebSocket):
 if __name__ == '__main__':
     server = ws4py.server.geventserver.WebSocketServer(
                 ('127.0.0.1', 9000),
-                websocket_class=Server)
+                websocket_class=Server,
+                certfile='../pki/data/certs/servercert.pem',
+                keyfile='../pki/data/private/serverkey.pem',
+                ca_certs='../pki/data/certs/cacert.pem',
+                cert_reqs=ssl.CERT_REQUIRED,
+                ssl_version=ssl.PROTOCOL_TLSv1)
     server.serve_forever()
